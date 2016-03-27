f56e70bca168b8ccac64461b491e57afa7d02201
accraze <accraze@gmail.com>
2016-03-27 16:50:09 -0700

    updated label action docs

--- src/Discogs/Actions/Label.hs
+++ src/Discogs/Actions/Label.hs
@@ -1,4 +1,4 @@
--- | Contains master-related actions, like finding a label
+-- | Contains label related actions, like finding a label by id or getting a list of all the label's releases.
 module Discogs.Actions.Label
   ( getLabel, getLabelReleases ) where
 
@@ -11,10 +11,22 @@ import Data.Default.Class
 import Data.Aeson
 import qualified Data.Text as Text
 
--- | Get the information Discogs exposes on master with the specified id
+-- | Get a label with a specific id
+--
+-- GET \/labels\/:labelId
+--
+-- @
+--     runDiscogsAnon $ Discogs.Actions.getLabel $ LabelID "1"
+-- @
 getLabel :: Monad m => LabelID -> DiscogsT m Label
 getLabel = runRoute . Route.getLabel
 
--- | Get the information Discogs exposes on labl releases with the specified id
+-- | Get all releases from a label with a specific id
+--
+-- GET \/labels\/:labelId\/releases
+--
+-- @
+--     runDiscogsAnon $ Discogs.Actions.getLabelReleases $ LabelID "1"
+-- @
 getLabelReleases :: Monad m => LabelID -> DiscogsT m LabelReleaseList
 getLabelReleases = runRoute . Route.getLabelReleases
