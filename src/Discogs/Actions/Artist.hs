ec35cbc9d51d31ca66f13e8390bc27134a17a294
accraze <accraze@gmail.com>
2016-03-27 17:01:34 -0700

    updated artist action docs

--- src/Discogs/Actions/Artist.hs
+++ src/Discogs/Actions/Artist.hs
@@ -1,4 +1,4 @@
--- | Contains artist-related actions, like finding releases or retrieving an
+-- | Contains artist-related actions, like finding an artist's releases or retrieving an
 --   artist's information.
 module Discogs.Actions.Artist
   ( getArtist
@@ -15,10 +15,22 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 
 -- | Get the information Discogs exposes on artist with the specified id
+--
+-- GET \/artists\/:artistId
+--
+-- @
+--     runDiscogsAnon $ Discogs.Actions.getArtist $ ArtistID "108713"
+-- @
 getArtist :: Monad m => ArtistID -> DiscogsT m Artist
 getArtist = runRoute . Route.getArtist
 
 -- | Get all release information exposed for an artist with specified id
+--
+-- GET \/artists\/:artistId\/releases
+--
+-- @
+--     runDiscogsAnon $ Discogs.Actions.getArtistReleases $ ArtistID "108713"
+-- @
 getArtistReleases :: Monad m => ArtistID -> DiscogsT m ReleaseList
 getArtistReleases = runRoute . Route.getArtistReleases
 
