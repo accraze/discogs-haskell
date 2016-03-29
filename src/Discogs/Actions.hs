362a2fb5c27b395a3324e9c1dd359dbfdaf6eb99
accraze <accraze@gmail.com>
2016-03-28 23:20:27 -0700

    added docs for parent actions and types

--- src/Discogs/Actions.hs
+++ src/Discogs/Actions.hs
@@ -1,10 +1,12 @@
+-- | This module contains all actions that wrap Discog's API endpoints.
+-- Only database actions (with the exception of search) have been implemented so far.
 module Discogs.Actions
   ( module Discogs.Actions.Artist
   , module Discogs.Actions.Release
   , module Discogs.Actions.Master
   , module Discogs.Actions.Label ) where
 
-import Discogs.Actions.Artist
+import Discogs.Actions.Artist  
 import Discogs.Actions.Release
 import Discogs.Actions.Master
 import Discogs.Actions.Label
--- src/Discogs/Types.hs
+++ src/Discogs/Types.hs
@@ -1,3 +1,5 @@
+-- | This module contains all types of data returned by the Discogs API.
+-- Only database actions (with the exception of search) have been implemented so far.
 module Discogs.Types ( Artist, Release, Master, Label ) where
 
 import Discogs.Types.Artist (Artist)
